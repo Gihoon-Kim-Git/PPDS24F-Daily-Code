cf0fde32b8916aec9d40181cfa75c73e6d0a40bd
userName <doudousgirl@gmail.com>
2024-10-11 00:27:33 +0900

    week06 code

--- week06/28/Problem2.cpp
+++ week06/28/Problem2.cpp
@@ -6,7 +6,6 @@ int solution(vector<int> &A) {
     int N = A.size();
     vector<int> dp(N, -10001);  
     dp[0] = A[0];  
-
     for (int i = 1; i < N; i++) {
         for (int j = 1; j <= 6; j++) {
             if (i - j >= 0) {  
@@ -14,6 +13,5 @@ int solution(vector<int> &A) {
             }
         }
     }
-
     return dp[N - 1];  
 }
