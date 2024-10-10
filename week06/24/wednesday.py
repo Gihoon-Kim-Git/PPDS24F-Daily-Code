e31bff2e4465c807222aa4cd1ceb74fb278e561a
Jinkwan Jang <112927746+jinkwan1115@users.noreply.github.com>
2024-10-10 12:17:32 +0900

    Update wednesday.py

--- week06/24/wednesday.py
+++ week06/24/wednesday.py
@@ -1,9 +1,9 @@
-# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+        
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         # Helper function to check if a node is a leaf
