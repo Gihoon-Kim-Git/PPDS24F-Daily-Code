04bf3b65c723a1756525944d914329fda67ad553
Yeo Dong Youn <150418228+YeodongYoun1995@users.noreply.github.com>
2024-10-11 16:15:22 +0900

    Add files via upload

---
+++ week06/18/prob3.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if (not root.left) and (not root.right):
+            return 0
+        if (not root.left.left) and (not root.left.right):
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        else:
+            return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
