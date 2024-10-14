1f0dcc418688d52ebee2855ccd37a4963a0afa07
dhwodnjs <jaewon.oh@snu.ac.kr>
2024-10-14 13:59:02 +0900

    skeleton

---
+++ week07/17/problem1.cpp
@@ -0,0 +1,38 @@
+// week 07
+
+// Monday. [Pascal's triangle](https://leetcode.com/problems/pascals-triangle-ii/description/)
+
+
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+// Example 1:
+
+// Input: rowIndex = 3
+// Output: [1,3,3,1]
+// Example 2:
+
+// Input: rowIndex = 0
+// Output: [1]
+// Example 3:
+
+// Input: rowIndex = 1
+// Output: [1,1]
+ 
+
+// Constraints:
+
+// 0 <= rowIndex <= 33
+
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        
+    }
+};
