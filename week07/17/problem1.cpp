9311b14eb88becb0baeca93b72ae51049aaed18c
dhwodnjs <jaewon.oh@snu.ac.kr>
2024-10-16 11:54:15 +0900

    week07 problem1

--- week07/17/problem1.cpp
+++ week07/17/problem1.cpp
@@ -31,8 +31,42 @@ using namespace std;
 
 
 class Solution {
+
 public:
+
+    vector<int> prevRow = {1};
+    vector<int> currRow;
+
     vector<int> getRow(int rowIndex) {
+
+        if (rowIndex == 0){
+            return {1};
+        } else{
+            for (int i = 1; i <= rowIndex; i++){
+                currRow = {1};
+                for (int j = 1; j < i; j++){
+                    currRow.push_back(prevRow[j - 1] + prevRow[j]);
+                }
+                currRow.push_back(1);
+                prevRow = currRow;
+            }
+        }
+
+        return currRow;
+
+        // for (int i = 0; i <= rowIndex; i++){
+        //     result.push_back(getNum(rowIndex, i));
+        // }
+        
         
     }
+
+    // int getNum(int rowIndex, int idx){
+    //     // recursive -> time limit exceeded
+    //     if (idx == 0 || idx == rowIndex){
+    //         return 1;
+    //     } 
+
+    //     return getNum(rowIndex - 1, idx - 1) + getNum(rowIndex - 1, idx);
+    // }
 };
