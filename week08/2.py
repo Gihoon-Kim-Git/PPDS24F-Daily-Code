43183276aac11fed4c51beb5d398e65f7ba4f11a
Minwoo_macmini <pcu119@naver.com>
2024-10-24 18:23:21 +0900

    Submit week08

---
+++ week08/2.py
@@ -0,0 +1,14 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.schedules = []
+        return None
+        
+
+    def book(self, start: int, end: int) -> bool:
+        n = start
+        while n <= end:
+            if n <= e or s <= end:
+                return NotImplementedError
+                
+        
