490da398035fc851b86cfb9f990fd72b867225c2
Gihoon-Kim-Git <gihoon.kim@snu.ac.kr>
2024-10-27 19:10:04 +0900

    Delete week08/1.py

--- week08/1.py
+++
@@ -1,9 +0,0 @@
-def solution(K, A):
-    count = 0
-    current_length = 0
-
-    for a in A:
-        current_length += a
-        if current_length >= K:
-            count += 1
-            current_length = 0 
