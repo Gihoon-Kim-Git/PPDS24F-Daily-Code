2c4cd596a7f4136aa9b4313bc72016495d1059ef
김용민 <yongmin@kim-yongmin-ui-MacBookPro.local>
2024-11-15 02:06:33 +0900

    Code for week11

---
+++ week11/2/Prob2.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> climbcost;
+        int N = cost.size();
+        
+        climbcost.push_back(cost[0]);
+        climbcost.push_back(cost[1]);
+        if(N < 3){
+            return *min_element(climbcost.begin(), climbcost.end());
+        }else{
+            int i = 2;
+            while(N-1 > i){
+                climbcost.push_back(min(climbcost[i-1], climbcost[i-2])+cost[i]);
+                i++;
+
+            }
+                
+            return min(climbcost[-1], climbcost[-2]+cost[-1]);
+
+        }
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> cost;
+    cost.push_back(10);
+    cost.push_back(15);
+    cost.push_back(20);
+    sol.minCostClimbingStairs(cost);
+}
--- week11/2/Prob2.py
+++ week11/2/Prob2.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         climbcost = []
