ef32136e7d6da1c217df4b01fa28e2db0c6d0369
FranciscoPark <76905169+FranciscoPark@users.noreply.github.com>
2024-11-19 12:42:35 +0900

    Add files via upload

---
+++ week12/12/problem4.py
@@ -0,0 +1,27 @@
+class MyQueue:
+
+    def __init__(self):
+        self.q= []
+        
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        ans= self.q[0]
+        self.q.pop(0)
+        return ans
+
+    def peek(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q)==0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
