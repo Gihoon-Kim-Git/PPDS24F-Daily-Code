ccdad1814353020b203d7534ff804db34a1d61ee
YoojinKwon <102570293+YoojinKwon@users.noreply.github.com>
2024-11-22 15:28:58 +0900

    code for week12

---
+++ week12/1/p225.py
@@ -0,0 +1,16 @@
+class MyStack(object):
+
+    def __init__(self):
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
---
+++ week12/1/p232.py
@@ -0,0 +1,16 @@
+class MyQueue(object):
+
+    def __init__(self):
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
